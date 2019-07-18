2508edc0702557fddcd5183c87a4dbc20060a8df
aaaaus <ausluft@gmail.com>
2019-07-18 00:29:43 -0400

    new notes for variables, execution context

---
+++ js/execution_context.js
@@ -0,0 +1,36 @@
+//EXECUTION CONTEXT
+
+//the environment in which JavaScript code is executed
+
+//Global Execution Context
+//Functional Execution Context
+
+//EXECUTION STACK ("CALL STACK")
+
+//it is a stack data structure (stackframe)
+//global execution context is first (bottom) in stack
+
+
+//CREATION PHASE vs EXECUTION PHASE
+
+//CREATION:
+
+//GLOBAL:
+//1 - Create a global object.
+//2 - Create an object called “this”.
+//3 - Set up memory space for variables and functions.
+//4 - Assign variable declarations a default value of “undefined” while placing any function declarations in memory.
+
+//FUNCTIONAL:
+//1 - Create an arguments object.
+//2 - Create an object called "this".
+//3 - Set up memory space for variables and functions.
+//4 - Assign variable declarations a default value of “undefined” while placing any function declarations in memory.
+
+//EXECUTION:
+//run code line by line and execute it
+
+
+//HOISTING
+
+//The process of assigning variable declarations a default value of undefined during the creation phase
---
+++ js/variables.js
@@ -0,0 +1,37 @@
+//DECLARATION vs ASSIGNMENT
+
+//We declare and assign, and the Javascript engine initializes.
+
+//declaration:
+
+let predator //declaration, initialized as 'undefined'
+predator = 'lion' //assignment
+
+let prey = 'gazelle' //declaration and assignment
+prey = 'zebra' //reassignment
+
+var weather = 'hot' //declaration and assignment
+var weather = 'rainy' //redeclaration and reassignment
+
+//due to hoisting, variables are initialized with 'undefined' values during creation phase
+//variables are then assigned appropriate values during execution phase
+
+
+
+//VAR
+
+//function scoped
+//hoisting: undefined when accessing a variable before it is declared
+
+//LET
+
+//block scoped
+//hoisting: ReferenceError when accessing a variable before it is declared
+//cannot be redeclared
+
+//CONST
+
+//block scoped
+//hoisting: ReferenceError when accessing a variable before it is declared
+//cannot be redeclared
+//cannot be assigned a new value, but is NOT immutable
