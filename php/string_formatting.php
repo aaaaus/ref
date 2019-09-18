cbe63ad4b7677ab2297895e38cb2887f104f7ea0
aaaaus <42099359+aaaaus@users.noreply.github.com>
2019-09-18 13:04:58 -0400

    Create string_formatting.php

---
+++ php/string_formatting.php
@@ -0,0 +1,8 @@
+<?php
+
+$mood = 'hungry';
+$action = 'eat pasta';
+
+$sentence = sprintf('I am feeling %s and I want to %s.', $mood, $action);
+
+//return: 'I am feeling hungry and I want to eat pasta'
