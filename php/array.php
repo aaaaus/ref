b327b5189611c21ab0014e5f7fd82dd250885807
aaaaus <42099359+aaaaus@users.noreply.github.com>
2019-09-23 10:22:03 -0400

    Create array.php

---
+++ php/array.php
@@ -0,0 +1,27 @@
+//working with arrays 
+
+//array_map
+
+$names = ['jon','alexis','sammy'];
+
+$formatted_names = array_map(function($name) {
+  return ucfirst($name);
+}, $names);
+
+print_r($formatted_names);
+//(
+//  [0] => Jon
+//  [1] => Alexis
+//  [2] => Sammy
+//)
+
+//range
+
+range(7,11);
+//(
+//  [0] => 7
+//  [1] => 8
+//  [2] => 9
+//  [3] => 10
+//  [4] => 11
+//)
