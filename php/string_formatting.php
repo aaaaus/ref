099419362e8a16e3f5cd9335cad90bdc61f751bc
aaaaus <42099359+aaaaus@users.noreply.github.com>
2019-09-23 10:24:55 -0400

    Update string_formatting.php

--- php/string_formatting.php
+++ php/string_formatting.php
@@ -6,3 +6,15 @@ $action = 'eat pasta';
 $sentence = sprintf('I am feeling %s and I want to %s.', $mood, $action);
 
 //return: 'I am feeling hungry and I want to eat pasta'
+
+ucfirst('new york'); //converts the first character of a string to uppercase
+//return: 'New york'
+
+lcfirst('Reddit'); //converts the first character of a string to lowercase
+//return: 'reddit'
+
+strtoupper('hello'); //converts a string to uppercase
+//return 'HELLO'
+
+strtolower('QUIET'); //converts a string to lowercase
+//return 'quiet'
