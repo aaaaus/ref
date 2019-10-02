3af05cb555f2b03842e3017efa6654e503457662
aaaaus <42099359+aaaaus@users.noreply.github.com>
2019-10-02 17:10:02 -0400

    Update time.php

--- php/time.php
+++ php/time.php
@@ -7,6 +7,22 @@
 date('m/d/y');
 //string: '09/20/19'
 
+//list of format characters: https://www.php.net/manual/en/function.date.php
+
+//common:
+// Y (4 digit year)
+// y (2 digit year)
+
+//m (numeric month with leading zeros)
+
+//d (day of the month w/ leading zeros)
+
+// H (24 hour format of hour w/ leading zeros)
+// i (minutes w/ leading zeros)
+// s (seconds w/ leading zeros)
+
+
+
 $halloween = '10/31/19'
 
 //strtotime returns a unix timestamp for a date string passed in
