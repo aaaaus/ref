e20ffa759c8f6795e5852d7c578661d46c8ac45a
aaaaus <42099359+aaaaus@users.noreply.github.com>
2019-10-04 16:14:30 -0400

    Create scope.php

---
+++ php/scope.php
@@ -0,0 +1,7 @@
+<?php
+
+//get_defined_vars 
+
+//Calling get_defined_vars() will return a multidimensional array containing 
+//all defined variables (environment, server or user-defined) within the scope that it is called
+
