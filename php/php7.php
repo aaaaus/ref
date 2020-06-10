cb978239854d9561c853bc0eb62471dfde7eaea9
aaaaus <42099359+aaaaus@users.noreply.github.com>
2020-06-10 11:09:41 -0400

    Update php7.php

--- php/php7.php
+++ php/php7.php
@@ -12,6 +12,21 @@ $username = isset($_GET['user']) ? $_GET['user'] : 'nobody';
 // 'nobody'.
 $username = $_GET['user'] ?? $_POST['user'] ?? 'nobody';
 
+// spaceship operator
 
+//The spaceship operator is used for comparing two expressions. It returns -1, 0 or 1 when $a is respectively less than, equal to, or greater than $b
 
-?>
+// Integers
+echo 1 <=> 1; // 0
+echo 1 <=> 2; // -1
+echo 2 <=> 1; // 1
+
+// Floats
+echo 1.5 <=> 1.5; // 0
+echo 1.5 <=> 2.5; // -1
+echo 2.5 <=> 1.5; // 1
+ 
+// Strings
+echo "a" <=> "a"; // 0
+echo "a" <=> "b"; // -1
+echo "b" <=> "a"; // 1
